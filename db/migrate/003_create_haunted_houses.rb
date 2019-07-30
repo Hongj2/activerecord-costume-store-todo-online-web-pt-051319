b57223f96bf435d2d0e116c42307cdddb7a460d6
Jessica Hong <jessicahong.ngnr@gmail.com>
2019-07-30 00:32:40 +0000

    Automatically backed up by Learn

--- db/migrate/003_create_haunted_houses.rb
+++ db/migrate/003_create_haunted_houses.rb
@@ -1,2 +1,18 @@
 # Create your haunted_houses migration here
 
+
+class CreateHauntedHouses < ActiveRecord::Migration[4.2]
+  def change 
+    create_table :haunted_houses do |t|
+  t.string :name
+  t.string :location
+  t.integer :costume_inventory
+  t.integer :number_of_employees
+  t.boolean :family_friendly
+  t.datetime :opening_time
+  t.datetime :closing_time 
+  
+  t.timestamp null:false
+    end
+  end
+end
