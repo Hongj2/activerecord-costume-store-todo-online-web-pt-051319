4f91681eb9e6dcde6b35207707db4087a6f8b2b5
Jessica Hong <jessicahong.ngnr@gmail.com>
2019-07-30 00:28:18 +0000

    Automatically backed up by Learn

--- db/migrate/002_create_costume_stores.rb
+++ db/migrate/002_create_costume_stores.rb
@@ -1,2 +1,14 @@
 # Create your costume_stores migration here
 
+class CostumeStore < ActiveRecord::Migration[4.2]
+  def change 
+    create_table :Costume do |t|
+  t.string :name
+  t.float :price
+  t.string :size
+  t.string :image_url 
+  
+  t.timestamp null:false
+    end
+  end
+end
