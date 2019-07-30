a970c3fcf54579ce12d425abfd7014055520e22a
Jessica Hong <jessicahong.ngnr@gmail.com>
2019-07-30 00:27:05 +0000

    Automatically backed up by Learn

--- db/migrate/001_create_costumes.rb
+++ db/migrate/001_create_costumes.rb
@@ -12,8 +12,8 @@ class CostumeStore < ActiveRecord::Migration[4.2]
   def change 
     create_table :Costume do |t|
   t.string :name
-  t.integer :price
-  t.integer :size
+  t.float :price
+  t.string :size
   t.string :image_url 
   end
   
