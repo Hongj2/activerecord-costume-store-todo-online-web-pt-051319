b87fcf73582389f8d3c9f94e078b7f2beaef4ccb
Jessica Hong <jessicahong.ngnr@gmail.com>
2019-07-30 01:08:41 +0000

    Automatically backed up by Learn

--- db/migrate/003_create_haunted_houses.rb
+++ db/migrate/003_create_haunted_houses.rb
@@ -10,7 +10,7 @@ class CreateHauntedHouses < ActiveRecord::Migration[4.2]
   t.float :price
   t.boolean :family_friendly?
   t.datetime :opening_time
-  t.datetime :closing_time 
+  t.datetime :closing_date 
   t.string :description 
   
   t.timestamp null:false
